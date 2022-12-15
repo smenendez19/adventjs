d7105f327443b2dc255fb196845b5c4de1d9f082
Santiago Menendez <santiagomenendez@outlook.com>
2022-12-15 16:24:42 -0300

    Fixed challenge 15

--- 2022/challenge15/solution.js
+++ 2022/challenge15/solution.js
@@ -1,24 +1,23 @@
 function decorateTree(base) {
     let tree = [base.split(" ")]
     const dict = {
-        "PP" : "P",
-        "BB" : "B",
-        "RR" : "R",
-        "BP" : "R",
-        "PB" : "R",
-        "BR" : "P",
-        "RB" : "P",
-        "PR" : "B",
-        "RP" : "B"
+        "PP": "P",
+        "BB": "B",
+        "RR": "R",
+        "BP": "R",
+        "PB": "R",
+        "BR": "P",
+        "RB": "P",
+        "PR": "B",
+        "RP": "B"
     }
-    tree[0].slice(1).forEach(() => {
-        let row = []
-        tree[0].slice(1).forEach((_, j) => {
-            row.push(dict[tree[0][j] + tree[0][j + 1]])
-        })
-        tree.unshift(row)
-    })
+    Array(base.split(" ").length - 1).fill().forEach(() =>
+        tree.unshift(
+            tree[0].slice(1).map((_, j) =>
+                dict[tree[0][j] + tree[0][j + 1]])))
     return tree.map(row => row.join(" "))
 }
+
+console.log(decorateTree('B P R P'))
 
 module.exports = decorateTree
