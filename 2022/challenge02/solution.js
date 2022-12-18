7c5ace6f92e82c917532c9192bf12d155eb81c78
Santiago Menendez <santiagomenendez@outlook.com>
2022-12-18 08:58:40 -0300

    Fixed challenge 02

--- 2022/challenge02/solution.js
+++ 2022/challenge02/solution.js
@@ -1,6 +1,5 @@
 function countHours(year, holidays) {
-  return holidays.map((item) => {
-    const weekDay = new Date(year + "/" + item).getDay()
-    return (weekDay === 0 || weekDay === 6) ? 0 : 2
-  }).reduce((a, b) => a + b)
+  return holidays.map((item) =>
+    [0, 6].includes(new Date(year + "/" + item).getDay()) ? 0 : 2
+  ).reduce((a, b) => a + b)
 }
