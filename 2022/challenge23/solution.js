4f11c7171624a1fe0b429588a7e019dee7b6d43f
Santiago Menendez <santiagomenendez@outlook.com>
2022-12-24 04:47:36 -0300

    Fix challenge 23, better solution

--- 2022/challenge23/solution.js
+++ 2022/challenge23/solution.js
@@ -1,12 +1,12 @@
 function executeCommands(commands) {
     let registers = Array(8).fill(0)
+    let i = 0
     const actions = {
         "MOV": (args) => {
-            args[1] = Number(args[1].slice(1))
-            args[0] = args[0][0] === "V" ?
-                registers[Number(args[0].slice(1))]
+            args[0] = args[0][0] === "V" ? 
+                registers[Number(args[0].slice(1))] 
                 : Number(args[0])
-            registers[args[1]] = args[0]
+            registers[Number(args[1].slice(1))] = args[0]
         },
         "ADD": (args) => registers[Number(args[0].slice(1))] =
             (registers[Number(args[0].slice(1))] +
@@ -16,15 +16,13 @@ function executeCommands(commands) {
             (--registers[Number(args[0].slice(1))] + 256) % 256,
         "INC": (args) => registers[Number(args[0].slice(1))] =
             ++registers[Number(args[0].slice(1))] % 256,
-        "JMP": (args, i) => registers[0] != 0 ? Number(args[0]) - 1 : i
+        "JMP": (args) => i = 
+            ((Number(args[0]) - 1) * (registers[0] != 0)) + 
+            (i * (registers[0] == 0))
     }
-    for (let i = 0; i < commands.length; i++) {
+    for (; i < commands.length; i++) {
         const command = commands[i].split(" ")[0]
         const args = commands[i].split(" ")[1].split(",")
-        if (command === "JMP") {
-            i = actions[command](args, i)
-            continue
-        }
         actions[command](args)
     }
     return registers
