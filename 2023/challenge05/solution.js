3b8d5a7f521ec37d62a838c90e6d6225ec2106a2
Santiago Menendez <santiagomenendez@outlook.com>
2023-12-19 23:15:20 -0300

    Fix challenge 5

--- 2023/challenge05/solution.js
+++ 2023/challenge05/solution.js
@@ -3,12 +3,11 @@ function cyberReindeer(road, time) {
     let timePassed = 0
     let pos = 0
     let aux = "."
-    while (time > 0) {
+    while (timePassed < time) {
         result.push(road)
-        time--
         timePassed++
         if (timePassed >= 5) road = road.replace(/\|/gi, "*")
-        if (road[pos+1] === '.' || road[pos+1] === '*') {
+        if ([".", "*"].includes(road[pos+1])) {
             road = road.split("")
             road[pos] = aux
             aux = road[pos+1]
