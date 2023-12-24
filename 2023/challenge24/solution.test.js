864af07db105481e338bd7c90787afa43092edab
Santiago Menendez <santiagomenendez@outlook.com>
2023-12-24 16:56:59 -0300

    Fix tests

--- 2023/challenge24/solution.test.js
+++ 2023/challenge24/solution.test.js
@@ -1,16 +1,16 @@
 const getStaircasePaths = require('./solution.js')
 
-//test('Test Case 1', () => {
-//    expect(getStaircasePaths(1, 1)).toEqual([[1]])
-//})
-//
-//test('Test Case 2', () => {
-//    expect(getStaircasePaths(0, 0)).toEqual([[]])
-//})
-//
-//test('Test Case 3', () => {
-//    expect(getStaircasePaths(2, 1)).toEqual([[1, 1]])
-//})
+test('Test Case 1', () => {
+    expect(getStaircasePaths(1, 1)).toEqual([[1]])
+})
+
+test('Test Case 2', () => {
+    expect(getStaircasePaths(0, 0)).toEqual([[]])
+})
+
+test('Test Case 3', () => {
+    expect(getStaircasePaths(2, 1)).toEqual([[1, 1]])
+})
 
 test('Test Case 4', () => {
     expect(getStaircasePaths(4, 2)).toEqual([
